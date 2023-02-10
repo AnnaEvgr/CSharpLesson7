7a81d88680db235f708b438f04093e5b7d1df599
AnnaEvgrafova <eaa79evgrafova@yandex.ru>
2023-02-10 19:58:42 +0500

    Урок 7

---
+++ Task 1/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+
+0,5 7 -2 -0,2
+
+1 -3,3 8 -9,9
+
+8 7,8 -7,1 9*/
+
+Console.WriteLine("Введите количество строк");
+int i = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int j = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = new double[i, j];
+FillArray(matrix);
+PrintArray(matrix);
+
+void FillArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = Convert.ToDouble(new Random().Next(-10, 10)) / 10;
+        }
+    }
+}
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
---
+++ Task 3/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задача 3. Задайте двумерный массив из целых чисел. 
+Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+Console.WriteLine("Введите размер матрицы:");
+int N = int.Parse(Console.ReadLine());
+int M = int.Parse(Console.ReadLine());
+int[,] matrix = new int[N, M];
+Random rand = new Random();
+int[] summ = new int[M];
+for (int i = 0; i < N; i++)
+{
+    for (int j = 0; j < M; j++)
+    {
+        matrix[i, j] = rand.Next(0, 10);
+    }
+}
+Console.WriteLine("Матрица");
+for (int i = 0; i < N; i++)
+{
+    for (int j = 0; j < M; j++)
+    {
+        Console.Write($"{matrix[i, j]}\t");
+        summ[i] += matrix[j, i];
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+foreach (double elem in summ)
+{
+    Console.WriteLine($"Среднее арифметическое {elem / M}");
+}
